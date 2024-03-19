d8ebd1891022bab6f8c7f3a752c4dac27d10d4e5
kkm4512 <nayoun440@naver.com>
2024-03-19 20:36:36 +0900

    1
    1

--- B/backend/src/main.ts
+++ B/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//1
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
